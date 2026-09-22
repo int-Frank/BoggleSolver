4a8cb90ad0a0449844d6b05405854259f38b3ca7
Frank Hart <frankhart010@gmail.com>
2026-09-22 11:24:23 +1000

    Added simple ImGui window

--- premake5.lua
+++ premake5.lua
@@ -45,6 +45,16 @@ workspace "BoggleSolver"
     {
       "Application/src/**.h",
       "Application/src/**.cpp",
+      "Application/3rdParty/ImGui/imgui.cpp",
+      "Application/3rdParty/ImGui/imgui.h",
+      "Application/3rdParty/ImGui/imgui_internal.h",
+      "Application/3rdParty/ImGui/imgui_draw.cpp",
+      "Application/3rdParty/ImGui/imgui_tables.cpp",
+      "Application/3rdParty/ImGui/imgui_widgets.cpp",
+      "Application/3rdParty/ImGui/backends/imgui_impl_sdl3.h",
+      "Application/3rdParty/ImGui/backends/imgui_impl_sdl3.cpp",
+      "Application/3rdParty/ImGui/backends/imgui_impl_vulkan.h",
+      "Application/3rdParty/ImGui/backends/imgui_impl_vulkan.cpp",
     }
 
     includedirs
@@ -52,10 +62,37 @@ workspace "BoggleSolver"
       "Engine/src",
     }
 
+    externalincludedirs
+    {
+      "Application/3rdParty/SDL3/include",
+      "Application/3rdParty/ImGui",
+      "Application/3rdParty/ImGui/backends",
+      "$(VULKAN_SDK)/Include",
+    }
+
+    libdirs
+    {
+      "Application/3rdParty/SDL3/lib/x64",
+      "$(VULKAN_SDK)/Lib",
+    }
+
     links
     {
       "Engine",
+      "SDL3",
+      "vulkan-1",
     }
+
+    postbuildcommands
+    {
+      '{COPY} "%{wks.location}/Application/3rdParty/SDL3/lib/x64/SDL3.dll" "%{cfg.targetdir}"',
+    }
+
+    -- ImGui/backends are third-party sources; don't hold them to this project's
+    -- warnings-as-errors policy.
+    filter "files:Application/3rdParty/**"
+      warnings "Default"
+      buildoptions { "/WX-" }
 
     filter "configurations:Debug"
       runtime "Debug"
